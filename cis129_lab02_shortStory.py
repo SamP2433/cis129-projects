26afca5bbf5fd0848b68d286a36a38367aa92b98
SamP2433 <spena18@mail.pima.edu>
2024-09-13 15:16:04 -0700

    Made a python code that prints a short story about a person named Jim and his dounts

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello, World!')
+# Author: Samuel Pena
+# My first Python lab in my CIS129 course
+# Prints a simple short story about Jim and his glazed donut boxes
+
+print('Jim woke up at 11 am, he started his day by going on Instagram')
+print('Jim had saw a ad for Krispy Kreme, it was a deal for friday the 13th')
+print('Jim went to krispy kreme to get this deal and got 2 boxes of glazed donuts')
+print('On the way home, Jim gotten out of his car, however in the process he dropped both boxes of donuts onto the floor')
